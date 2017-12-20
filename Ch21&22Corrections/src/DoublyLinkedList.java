8694671d2a6d1b03f7e2f344e590ed3324b23d1d
"[MurphyPOne]" <"[murphyp1@bishopireton.org]">
2017-12-19 20:06:24 -0500

    AAARGHH

--- Ch21&22Corrections/src/DoublyLinkedList.java
+++ Ch21&22Corrections/src/DoublyLinkedList.java
@@ -1,6 +1,6 @@
 public class DoublyLinkedList {
 	private ListNode2 head;
-	private ListNode2 tail; //tail does not work
+	private ListNode2 tail; 
 	
 	public DoublyLinkedList(ListNode2 hed) {
 		head = hed;
@@ -15,20 +15,37 @@ public class DoublyLinkedList {
 	public ListNode2 getHead() { return head; }
 	public ListNode2 getTail() { return tail; }
 	
+	public void addLast(Object val) {
+		ListNode2 node = new ListNode2(val, tail, null);
+		if(head == null) {	//If empty
+			head = node;
+			tail = node;		//both head and tail point to the same node
+		} else 
+			tail.setNext(node);	//add a node after last
+		tail = node;		//change last to that node
+	}
+	
+	public DoublyLinkedList(Object[] values) { 
+		for(Object value: values) {
+			addLast(value);
+		}
+	}
+	
+	
 	/* #11 Sevan is bad at coding, fix his backlinks
 	 */
-	private void traverseBackwards() {
+	public void traverseBackwards() {
 		for(ListNode2 node = tail; node != null; node = node.getPrevious() ) {
 			System.out.println(node.getValue());
 		}
 	}
 	
-	private void fixBackLinks() {
+	public void fixBackLinks() {
 		for(ListNode2 node = head; node.getNext() != null; node = node.getNext() ) {
 			ListNode2 prev = node;	//Get temp value
 			node = node.getNext();	//move forward because those links work
 			node.setPrevious(prev);	//set prev = temp
-			node = node.getPrevious();	//go back so as not to double iterate, which should work bc I just fixed
+			node = node.getPrevious();	//go back so as not to double iterate, which should work bc I just fixed prev
 		}
 	}
 }
--- Ch21&22Corrections/src/Main.java
+++ Ch21&22Corrections/src/Main.java
@@ -2,6 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
+		//Singly Linked List
+		
 		String[] objs = new String[5];
 			objs[0] = "one";
 			objs[1] = "two";
@@ -11,22 +13,39 @@ public class Main {
 		
 		SinglyLinkedList numbers = new SinglyLinkedList(objs);
 		
-		System.out.println("After creation");
+		/*System.out.println("After creation");
 		System.out.println(numbers);
 		
 		System.out.println("\nTry Removal\n");
-		System.out.println(numbers.remove("one") );
+		System.out.println(numbers.remove("one") );		//Part 1 #10
 		System.out.println(numbers +"\n");
+		*/
 		
-		///STACK TESTING
 		
-		Stack nums = new Stack();
+		//DoublyLinkedList
+		
+		String[] objs2 = new String[5];
+			objs2[0] = "aaa";
+			objs2[1] = "bbb";
+			objs2[2] = "ccc";
+			objs2[3] = "ddd";
+			objs2[4] = "eee";
+		DoublyLinkedList letters = new DoublyLinkedList(objs2);
+		letters.traverseBackwards();	//before fix
+		System.out.println("\n 	BackLinks Fixed \n");
+		letters.fixBackLinks(); //Part 1 #11
+		letters.traverseBackwards();
+		
+		
+		//STACK TESTING
+		
+		Stack nums = new Stack();		//Part 2 #14
 		
 		for(int i = 0; i < 1024; i ++) {
 			nums.push(i);
 		}
 		
-		System.out.print("heres the stack:\n" + nums);
+		//System.out.print("heres the stack:\n" + nums);
 		
 	}
 }
